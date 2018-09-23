d8067da71ee783f623d8d2fb7281b835c273c287
WAWAN.H <WAWAN.H@WAWAN>
2018-09-23 17:11:51 +0700

    this is a PBO task

---
+++ src/pbo3/pkg10117107/latihan1/muhammadbariqa/PBO310117107Latihan1Muhammadbariqa.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117107.latihan1.muhammadbariqa;
+
+/**
+ *
+ * @author
+ * Nama                 : Muhammad Bariq A
+ * Kelas                : PBO3
+ * NIM                  : 10117107
+ * Deskripsi Program    : Program ini berisi program untuk menampilkan
+ *                        value ke layar
+ */
+public class PBO310117107Latihan1Muhammadbariqa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "--------------------");
+        System.out.println("NAMA\t\t: Muhammad Bariq A");
+        System.out.println("ALAMAT\t\t: Jalan Moh Toha");
+        System.out.println("TEMPAT LAHIR\t\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t\t: 5 Februari 1999");
+        System.out.println("--------------------");
+        System.out.println("HOBI\t\t:  Main Game dan Males Malesan");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Soda");
+    }
+    
+}

819b70f55240c9202cbcbb014e944ac7b5567a6d
muhammadbariq <43340253+muhammadbariq@users.noreply.github.com>
2018-09-23 17:30:24 +0700

    Add files via upload

---
+++ latihan2/muhammadbariqa/PBO310117107Latihan2MuhammadBariqA.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117107.latihan2.muhammadbariqa;
+
+/**
+ *
+ * @author
+ *  Nama                 : Muhammad Bariq A
+ * Kelas                : PBO3
+ * NIM                  : 10117107
+ * Deskripsi Program    : Program ini berisi program untuk mendeklarasi dan
+ *                        pengaksesan variabel
+ */
+public class PBO310117107Latihan2MuhammadBariqA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //menampilkan hasil
+        System.out.println();
+        System.out.println("Isi Variabel Nilai = " + nilaiInt);
+        System.out.println("Isi Variabel PHI = " + PHI);
+        System.out.println("Isi Variabel Logika = " + nilaiLogika);
+        System.out.println("Isi Variabel Karakter = " + nilaiKarakter);
+    }
+    
+}

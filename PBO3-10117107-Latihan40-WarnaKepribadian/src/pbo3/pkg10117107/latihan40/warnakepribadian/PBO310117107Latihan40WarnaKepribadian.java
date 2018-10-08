494afdd74f1c52ef679a1735a3d4d81c70588594
Muhammad Bariq A <43340253+muhammadbariq@users.noreply.github.com>
2018-10-08 21:25:14 +0700

    Add files via upload

---
+++ PBO3-10117107-Latihan40-WarnaKepribadian/src/pbo3/pkg10117107/latihan40/warnakepribadian/PBO310117107Latihan40WarnaKepribadian.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117107.latihan40.warnakepribadian;
+
+/**
+ *
+ * @author
+ * Nama                 : Muhammad Bariq A
+ * Kelas                : PBO3
+ * NIM                  : 10117107
+ * Deskripsi Program    : Program ini berisi program untuk menampilkan warna 
+ *                        kepribadian
+ */
+public class PBO310117107Latihan40WarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Warna scanner = new Warna();
+        
+        scanner.ioWarnaMu(); 
+    }
+    
+}

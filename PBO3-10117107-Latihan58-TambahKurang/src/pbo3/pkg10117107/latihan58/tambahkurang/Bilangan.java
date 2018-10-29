45c0eb1eb5c1fadfb14b62e46eb079b8700be5d0
Muhammad Bariq A <43340253+muhammadbariq@users.noreply.github.com>
2018-10-29 19:32:19 +0700

    Add files via upload

---
+++ PBO3-10117107-Latihan58-TambahKurang/src/pbo3/pkg10117107/latihan58/tambahkurang/Bilangan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117107.latihan58.tambahkurang;
+
+/**
+ *
+ * @author
+ */
+public class Bilangan {
+    private int x;
+    private int y;
+    
+    public Bilangan(){
+        this.x = 3;
+        this.y = 4;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+   
+}
---
+++ PBO3-10117107-Latihan58-TambahKurang/src/pbo3/pkg10117107/latihan58/tambahkurang/PBO310117107Latihan58TambahKurang.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117107.latihan58.tambahkurang;
+
+/**
+ *
+ * @author
+ * Nama                 : Muhammad Bariq A
+ * Kelas                : PBO3
+ * NIM                  : 10117107
+ * Deskripsi Program    : Program ini berisi program untuk menampilkan hasil dari
+ *                        penambahan dan pengurangan
+ */
+public class PBO310117107Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JumlahBilangan jumlah = new JumlahBilangan();
+        SelisihBilangan selisih = new SelisihBilangan();
+
+        jumlah.tampilHasilJumlah();
+        selisih.tampilSelisih();
+    }
+    
+}
